0a2d81bc75c135474d311099724713c032e697b2
Robotics-PEC <robotics@pec.edu.in>
2023-01-03 14:30:17 +0530

    added image generation code

--- cv2test.py
+++ cv2test.py
@@ -1,1 +1,16 @@
-import cv2.cv2
+import cv2
+
+cap = cv2.VideoCapture(0)
+i = 0
+while True:
+    sucess , img = cap.read()
+
+    cv2.imshow("frame" , img)
+    if cv2.waitKey(1) == ord('c'):
+        cv2.imwrite(f'images/image{i}.jpg' , img)
+        i+=1
+    if cv2.waitKey(1) == ord('s'):
+        break
+
+cap.release()
+
